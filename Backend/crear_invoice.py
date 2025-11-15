c2c0298d4634b0ac4670cd98aa6548ef430fefd9
Kiwil <13.kristycuculg@gmail.com>
2025-11-15 00:00:09 -0600

    Update crear_invoice.py
    Signed-off-by: Kiwil <13.kristycuculg@gmail.com>

--- Backend/crear_invoice.py
+++ Backend/crear_invoice.py
@@ -3,15 +3,17 @@ import qrcode
 import base64
 import io
 
-
 def crear_invoice(amount, memo):
-    # Crear invoice en LNbits
     respuesta = crear_invoice_lnbits(amount, memo)
+
+    # Manejo de error
+    if "error" in respuesta:
+        return respuesta
 
     payment_request = respuesta["payment_request"]
     payment_id = respuesta["payment_hash"]
 
-    # Generar QR en base64
+    # Crear QR
     img = qrcode.make(payment_request)
     buffer = io.BytesIO()
     img.save(buffer, format="PNG")
@@ -22,6 +24,7 @@ def crear_invoice(amount, memo):
         "payment_id": payment_id,
         "qr_base64": qr_b64
     }
+
 
 def verificar_pago(payment_id):
     return consultar_pago_lnbits(payment_id)
